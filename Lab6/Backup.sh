0b7354f409a3cf25c387d534ddb43d587cf58b87
Deepthi-n1 <deepthinachari@gmail.com>
2025-03-20 10:20:03 +0530

    Update Backup.sh

--- Lab6/Backup.sh
+++ Lab6/Backup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+mkdir -p ~/Documents/Backup
+
+# To copy all C program files s
+cp ~/Documents/*.c ~/Documents/Backup/
+
+# To change directory 
+cd ~/Documents
+
+# To create .tar.gz 
+tar -czf Backup.tar.gz Backup
+
+# Delete
+rm -rf ~/Documents/Backup
+
+echo "Backup and restore process completed successfully."
